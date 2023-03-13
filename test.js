4929f60f8b5551e9cd61acadd90e586371be9797
jinhokim <llsoftll1@naver.com>
2023-03-13 16:16:38 +0900

    클릭버튼기능 초기

---
+++ test.js
@@ -0,0 +1,13 @@
+const $btnAdd = document.getElementById('btnAdd');
+$btnAdd.addEventListener('click', () => {
+  console.log('클릭');
+});
+
+const $btDel = document.getElementById('btDel');
+$btDel.addEventListener('click', () => {
+  console.log('삭제');
+});
+const $btUpdate = document.getElementById('btUpdate');
+$btUpdate.addEventListener('click', () => {
+  console.log('수정');
+});
