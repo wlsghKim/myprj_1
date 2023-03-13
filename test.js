d19fcc2e61f59802c4e9346bc6d82a512c6f000c
jinhokim <llsoftll1@naver.com>
2023-03-13 17:10:06 +0900

    삭제기능 추가

--- test.js
+++ test.js
@@ -2,12 +2,7 @@ const $btnAdd = document.getElementById('btnAdd');
 $btnAdd.addEventListener('click', () => {
   console.log('클릭');
 });
-
 const $btDel = document.getElementById('btDel');
 $btDel.addEventListener('click', () => {
   console.log('삭제');
-});
-const $btUpdate = document.getElementById('btUpdate');
-$btUpdate.addEventListener('click', () => {
-  console.log('수정');
 });
